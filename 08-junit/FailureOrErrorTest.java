c1d8dea5c2ed25c64db3e491499cb90a999e71e1
Mikhail Rybalko <147132802+MrBurzhuadotexe@users.noreply.github.com>
2024-12-10 03:09:50 +0200

    Create FailureOrErrorTest.java

---
+++ 08-junit/FailureOrErrorTest.java
@@ -0,0 +1,29 @@
+package put.io.testing.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class FailureOrErrorTest {
+
+    @Test
+    void test1() {
+        assertTrue(2 == 3);
+    }
+
+    @Test
+    void test2() {
+        Integer.parseInt("dwa");
+    }
+
+    @Test
+    void test3() {
+        try {
+            assertTrue(2 == 3);
+        } catch(Throwable t) {
+            t.printStackTrace();
+        }
+    }
+}
